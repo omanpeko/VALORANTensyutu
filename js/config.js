f4252ac36a80013de4a3fcf4d25a229227538ac8
omanpeko <79346126+omanpeko@users.noreply.github.com>
2025-09-05 20:31:29 +0900

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,16 @@
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyCQtwlV1p9IS7v9Mh_nZsc5r2svmJxdVH4",
+    authDomain: "apex-point.firebaseapp.com",
+    databaseURL: "https://apex-point.firebaseio.com",
+    projectId: "apex-point",
+    storageBucket: "apex-point.appspot.com",
+    messagingSenderId: "227733642808",
+    appId: "1:227733642808:web:edbd9591653cca720390cd",
+    measurementId: "G-442PLZ7SDB"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
---
+++ js/firebase-config.js
@@ -0,0 +1,16 @@
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyC3H_Bqqr6-Qfxo0RU4oBacMe5Ov-Ncmik",
+    authDomain: "valorant-6165a.firebaseapp.com",
+    databaseURL: "https://valorant-6165a-default-rtdb.firebaseio.com",
+    projectId: "valorant-6165a",
+    storageBucket: "valorant-6165a.appspot.com",
+    messagingSenderId: "618487824843",
+    appId: "1:618487824843:web:b6648b615ffc2d1e2eb64e",
+    measurementId: "G-FN26YPWWEV"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
